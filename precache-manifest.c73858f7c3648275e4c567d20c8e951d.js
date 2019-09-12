5ce006ffcc94fbf89e13acd0334a7fdd2602ce99
1Lacky <55225139+1Lacky@users.noreply.github.com>
2019-09-12 10:03:11 +0500

    Add files via upload

---
+++ precache-manifest.c73858f7c3648275e4c567d20c8e951d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9f217312375d4d6bc4dbab399b4c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a34a68c4a4a9670b77",
+    "url": "/static/css/main.4b459556.chunk.css"
+  },
+  {
+    "revision": "4f86de5d85af38938b39",
+    "url": "/static/js/2.be12e58a.chunk.js"
+  },
+  {
+    "revision": "77a34a68c4a4a9670b77",
+    "url": "/static/js/main.6c5c5464.chunk.js"
+  },
+  {
+    "revision": "ad054ea678ea18933477",
+    "url": "/static/js/runtime~main.9fe870bf.js"
+  }
+]);
